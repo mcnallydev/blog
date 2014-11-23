d22572608027faf4ca57a67f9df2b2ee15005c44
Paulo McNally <paulomcnally@gmail.com>
2014-11-22 19:12:37 -0600

    move heroku module to first line

--- core/index.js
+++ core/index.js
@@ -2,8 +2,8 @@
 // Orchestrates the loading of Ghost
 // When run from command line.
 
-var server = require('./server');
 var newrelic = require('newrelic');
+var server = require('./server');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
